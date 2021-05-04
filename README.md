8524a0b6978dcbc351c1cfd85c077e71cf53bd63
Johan <68628917+johan-naizu@users.noreply.github.com>
2021-05-04 17:30:02 +0530

    Update

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
+![cleverbotfreeapi](https://i.imgur.com/HEskou1.jpg)
+
 # aiocleverbot
- Async wrapper for cleverbot
+An async python wrapper for cleverbot.
+Does not require API KEY
+
+![Downloads](https://pepy.tech/badge/aiocleverbot) ![PyPI](https://img.shields.io/pypi/v/aiocleverbot) ![PyPI - Python Version](https://img.shields.io/pypi/pyversions/aiocleverbot) ![PyPI - License](https://img.shields.io/pypi/l/aiocleverbot)
+## Installation
+```pip
+pip install aiocleverbot
+```
+## Usage
+```python
+from aiocleverbot import cleverbot
+
+# Without context
+response=await cleverbot("Hello")
+print(response)
+
+# With context
+# Please note that context should include messages sent to Cleverbot as well as the responses
+response=await cleverbot("Bad", ["hi", "How are you?"])
+print(response)
--- setup.py
+++ setup.py
@@ -1,24 +1,26 @@
-from setuptools import setup, find_packages
+import setuptools
 
-with open('README.md', 'r', encoding='utf8') as f:
-	long_description = f.read()
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
-with open('requirements.txt') as f:
-	requirements = f.read().splitlines()
-
-setup(
-	name='aiocleverbot',
-	version='1.0.0',
-	author='Johan Naizu',
-	author_email='johan@naizu.in',
-	description='Async library for cleverbot',
-	long_description=long_description,
-	long_description_content_type='text/markdown',
-	url='https://github.com/johan-naizu/aiocleverbot',
-	packages=find_packages(),
-            classifiers=[
-                "Programming Language :: Python :: 3",
-                "License :: OSI Approved :: MIT License",
-            ],
-	install_requires=requirements
+setuptools.setup(
+    name="aiocleverbot",
+    version="0.1.1",
+    author="Johan Naizu",
+    author_email="johan@naizu.in",
+    description="Async library for cleverbot",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/johan-naizu/aiocleverbot",
+    project_urls={
+        "Bug Tracker": "https://github.com/johan-naizu/aiocleverbot/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
 )
